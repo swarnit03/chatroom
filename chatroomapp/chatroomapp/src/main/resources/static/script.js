c99197aaba8800b1a18cfa2f4b13c1863a97e8b8
Swarnit Soni <swarnitsoni1304@gmail.com>
2024-11-26 09:49:02 +0530

    upload

---
+++ chatroomapp/chatroomapp/src/main/resources/static/script.js
@@ -0,0 +1,3 @@
+$(document).ready(e=>{
+    alert("test")
+})
---
+++ chatroomapp/chatroomapp/src/test/java/com/chatroom/chatroomapp/ChatroomappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatroom.chatroomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
